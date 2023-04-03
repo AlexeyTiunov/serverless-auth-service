3cd7bc4e1dd70768320689bc8427f9921a9af24e
Oleksii_Tiunov <Oleksii.Tiunov@nordstrom.com>
2023-04-03 12:50:39 +0300

    Task 7 Authorizer

---
+++ src/functions/basicAuthorizer/handler.ts
@@ -0,0 +1,50 @@
+import * as process from "process";
+import {
+  APIGatewayAuthorizerResult,
+  APIGatewayTokenAuthorizerEvent,
+} from "aws-lambda/trigger/api-gateway-authorizer";
+
+const generatePolicy = (
+  principalId: string,
+  resource: string,
+  effect = "Allow"
+): APIGatewayAuthorizerResult => {
+  return {
+    principalId,
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+  };
+};
+
+export const basicAuthorizer = async (
+  event: APIGatewayTokenAuthorizerEvent
+) => {
+  console.log(event);
+  const { type, authorizationToken, methodArn } = event;
+  if (type !== "TOKEN") {
+    return "Unauthorized";
+  }
+  try {
+    const buff: Buffer = Buffer.from(authorizationToken, "base64");
+    const plainCreds: Array<string> = buff.toString("utf-8").split(":");
+    const [userName, password] = plainCreds;
+
+    if (!process.env[userName])
+      return generatePolicy(authorizationToken, methodArn, "Deny");
+
+    const effect: string =
+      process.env[userName] === password ? "Allow" : "Deny";
+
+    return generatePolicy(userName, methodArn, effect);
+  } catch {
+    return generatePolicy(authorizationToken, methodArn, "Deny");
+  }
+};
